40af648b43255133e4c02b21f688c1d0fe50fe40
Febin <febinaugustine7@gmail.com>
2024-09-03 19:22:45 +0530

    travio

---
+++ js/home.js
@@ -0,0 +1,8 @@
+function showHtmlDiv() {
+  var htmlShow = document.getElementById("html-show");
+  if (htmlShow.style.display === "none") {
+    htmlShow.style.display = "block";
+  } else {
+    htmlShow.style.display = "none";
+  }
+}
