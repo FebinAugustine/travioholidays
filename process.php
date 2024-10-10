154bb8b06b187ac8735b9ab15294ad752c611054
Febin <febinaugustine7@gmail.com>
2024-10-10 14:52:48 +0530

    last

--- process.php
+++ process.php
@@ -2,24 +2,24 @@
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-$phone = $post['phone'];
+$phone = $_POST['phone']; // Assuming you have a phone field in your form
 $message = $_POST['message'];
 
-$to = "travioholidayshussain@gmail.com";
+$to = "travioinfo@gmail.com";
 $subject = "Contact Form Submission";
 
 $body = "You have received a new contact form submission:\n\n";
 $body .= "Name: $name\n";
 $body .= "Email: $email\n\n";
-$body .= "Phone: $phone\n\n";
+$body .= "Phone: $phone\n\n"; // Include phone number if present
 $body .= "Message: \n$message";
 
 $headers = "From: $email";
 
 if (mail($to, $subject, $body, $headers)) {
-  echo "Your message has been sent successfully!";
+  // Success message and redirection using JavaScript
+  echo "<script>alert('Your message has been sent successfully!'); window.location.href = 'contact.html';</script>";
 } else {
   echo "There was an error sending your message. Please try again later.";
 }
 
-?>
